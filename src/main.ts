d5b82e112349c0cc8f4c61eadf79be4763a830b6
nakamuraos <thinhhoang3030@gmail.com>
2021-12-02 17:10:09 +0700

    [Nakamura] Update package version
    Signed-off-by: nakamuraos <thinhhoang3030@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import * as core from '@actions/core';
-import * as JobStatus from './status';
-import * as GoogleChat from './chat';
+import * as core from '@actions/core'
+import * as JobStatus from './status'
+import * as GoogleChat from './chat'
 
 async function run() {
   try {
-    const name = core.getInput('name', { required: true });
-    const url = core.getInput('url', { required: true });
-    const status = JobStatus.parse(core.getInput('status', { required: true }));
+    const name = core.getInput('name', { required: true })
+    const url = core.getInput('url', { required: true })
+    const status = JobStatus.parse(core.getInput('status', { required: true }))
 
-    core.debug(`input params: name=${name}, status=${status}, url=${url}`);
+    core.debug(`input params: name=${name}, status=${status}, url=${url}`)
 
-    await GoogleChat.notify(name, url, status);
+    await GoogleChat.notify(name, url, status)
     console.info('Sent message.')
-  } catch (error) {
-    core.setFailed(error.message);
+  } catch (error: any) {
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
