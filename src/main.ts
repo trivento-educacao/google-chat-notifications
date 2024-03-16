261d1ce834e3dc815cc91b2ae049699667c54d2d
ThinhHV <thinh@thinhhv.com>
2024-03-17 01:06:44 +0700

    add debug
    Signed-off-by: ThinhHV <thinh@thinhhv.com>

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,14 @@ async function run() {
     const status = JobStatus.parse(core.getInput('status', { required: true }))
     const threadKey = core.getInput('threadKey', { required: false })
 
-    core.debug(`input params: title=${title}, subtitle=${subtitle}, status=${status}, webhookUrl=${webhookUrl}, threadKey=${threadKey}`)
+    core.debug(
+      `input params: title=${title}, subtitle=${subtitle}, status=${status}, webhookUrl=${webhookUrl}, threadKey=${threadKey}`,
+    )
 
     await GoogleChat.notify({ title, subtitle, webhookUrl, status, threadKey })
     console.info('Sent message.')
   } catch (error: any) {
+    core.debug(error)
     core.setFailed(error.message)
   }
 }
